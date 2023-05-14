a104824febd5060ad0796395cd111329a9aee302
Ismail Ilhomov <ismoil6ek@gmail.com>
2023-05-14 17:59:39 +0500

    feat: theme toggle

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,52 +1,41 @@
-const header = document.getElementById("header");
-
-window.addEventListener("");
-
 function detectColorScheme() {
-  var theme = "light"; //default to light
+  let theme = "light";
 
   //local storage is used to override OS theme settings
   if (localStorage.getItem("theme")) {
-    if (localStorage.getItem("theme") == "dark") {
-      var theme = "dark";
+    if (localStorage.getItem("theme") === "dark") {
+      theme = "dark";
     }
   } else if (!window.matchMedia) {
-    //matchMedia method not supported
     return false;
   } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    //OS theme setting detected as dark
-    var theme = "dark";
+    theme = "dark";
   }
 
-  //dark theme preferred, set document with a `data-theme` attribute
-  if (theme == "dark") {
+  if (theme === "dark") {
     document.documentElement.setAttribute("data-theme", "dark");
   }
 }
+
 detectColorScheme();
 
-//identify the toggle switch HTML element
-const toggleSwitch = document.querySelector(
-  '#theme-switch input[type="checkbox"]'
+const toggleSwitch = document.querySelector("#theme-switch");
+
+function switchTheme(isDark) {
+  const theme = isDark ? "dark" : "light";
+  localStorage.setItem("theme", theme);
+  document.documentElement.setAttribute("data-theme", theme);
+  toggleSwitch.checked = isDark;
+}
+
+toggleSwitch.addEventListener(
+  "change",
+  (e) => switchTheme(e.target.checked),
+  false
 );
 
-//function that changes the theme, and sets a localStorage variable to track the theme between page loads
-function switchTheme(e) {
-  if (e.target.checked) {
-    localStorage.setItem("theme", "dark");
-    document.documentElement.setAttribute("data-theme", "dark");
-    toggleSwitch.checked = true;
-  } else {
-    localStorage.setItem("theme", "light");
-    document.documentElement.setAttribute("data-theme", "light");
-    toggleSwitch.checked = false;
-  }
-}
-
-//listener for changing themes
-toggleSwitch.addEventListener("change", switchTheme, false);
-
-//pre-check the dark-theme checkbox if dark-theme is set
-if (document.documentElement.getAttribute("data-theme") == "dark") {
-  toggleSwitch.checked = true;
-}
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    switchTheme(event.matches);
+  });
