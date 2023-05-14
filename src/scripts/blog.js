41fbead03071395a246c48374cb72e109cdf62bc
speedym282 <abdulaxadrizakulov@gmail.com>
2023-05-14 23:46:23 +0500

    add blog sow more feature

--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -70,13 +70,17 @@ filterbarButtons.forEach((e, i) => {
  * ---------------------------------------------------------------
  **/
 
-const blogPostsContainer = document.getElementById('blog-posts');
-const blogPost = ({ img, name, createdAt, viewCount, title, shortDesc }) => {
+const blogPostsContainerVisible = document.getElementById(
+  'blog-posts--visible'
+);
+const blogPostsContainerHidden = document.getElementById('blog-posts--hidden');
+
+const blogPost = ({ image, name, createdAt, viewCount, title, shortDesc }) => {
   return `
     <div class="blog-posts__card">
       <div
         class="blog-posts__card-head"
-        style="background: url('${img}')"
+        style="background-image: url('https://cdn.najottalim.uz/najottalim/${image}')"
       >
         <span class="blog-posts__card-name">${name}</span>
       </div>
@@ -85,7 +89,7 @@ const blogPost = ({ img, name, createdAt, viewCount, title, shortDesc }) => {
         <div class="blog-posts__card-info">
           <span class="card-info__text">${createdAt}</span>
           <span class="card-info__text">
-            <image class="card-info__icon" src="#" alt="Not Found" />
+            <image class="card-info__icon" src="../assets/svg/views.svg" alt="Not Found" />
             ${viewCount}
           </span>
         </div>
@@ -102,4 +106,24 @@ const blogPost = ({ img, name, createdAt, viewCount, title, shortDesc }) => {
   `;
 };
 
-blogPostsContainer.innerHTML = POSTS.map((e, i) => blogPost(e)).join('');
+const visiblePart = [];
+const hiddenPart = [];
+
+POSTS.forEach((e, i) => {
+  if (i < 6) visiblePart.push(e);
+  else hiddenPart.push(e);
+});
+
+blogPostsContainerVisible.innerHTML = visiblePart
+  .map((e) => blogPost(e))
+  .join('');
+
+blogPostsContainerHidden.innerHTML = hiddenPart
+  .map((e) => blogPost(e))
+  .join('');
+
+const showMore = document.getElementById('show-more-btn');
+showMore.addEventListener('click', () => {
+  blogPostsContainerHidden.style.display = 'flex';
+  showMore.style.display = 'none';
+});
